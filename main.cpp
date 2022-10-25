a17f6ba9823f98690718737c9cc6590a06a77c65
Andreo2 <andreo2.filippov00@gmail.com>
2022-10-26 00:22:43 +0300

    polar realisation

--- main.cpp
+++ main.cpp
@@ -1,55 +1,40 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 #include <cmath>
-#include <ctime>
 #include "mpi.h"
 #define TARGET 4.0*M_PI/3.0
-int main(int argc, char *argv[]){
-  int rank, size;
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  double y, z, volume = 8.0;
 
-  double tol = std::atof(argv[1]), eps = 0.0, acc_volume = 0.0, start_time, proc_sum = 0.0;
-  int iter = 1, stop = 1;
-  
-  int pack_size = 2000/size;
-  double delta = 2.0/size;
-  int N = size*pack_size;
-  start_time = MPI_Wtime();
-  srand(rank+1);
-  while(stop)
-  {
-    for(int i = 0; i < pack_size; ++i)
-    {
-      y = -1 + rank*delta + delta*(double)rand()/(RAND_MAX+1.0);
-      z = -1 + 2.0*(double)rand()/(RAND_MAX+1.0);
-      if (y*y + z*z <= 1)
-        proc_sum += sqrt(y*y + z*z);
+int main(int argc, char *argv[]) {
+    int i, rank, size, iter = 1, stop = 1, block_size = 20;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    double r, proc_sum = 0.0, acc_sum = 0.0, result = 0.0, eps = std::atof(argv[1]);
+    srand(rank + 1);
+    double start_time = MPI_Wtime();
+    while(stop){
+        for(i = 0; i < block_size; ++i)
+        {
+            r = (double)rand()/RAND_MAX;
+            proc_sum += r*r;
+        }
+        MPI_Allreduce(&proc_sum, &acc_sum, 1, MPI_DOUBLE,MPI_SUM, MPI_COMM_WORLD);
+        result = 4.0*M_PI*acc_sum/(iter*size*block_size);
+        if (std::fabs(TARGET - result) < eps){
+            stop = 0;
+        }
+        ++iter;
     }
-    MPI_Allreduce(&proc_sum, &acc_volume, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-    eps = std::abs(TARGET - volume*acc_volume/(iter*N));
-    if (eps < tol)
-    {
-      stop = 0;
-      acc_volume = TARGET + eps;
+    double end_time = MPI_Wtime();
+    double result_time, time = end_time - start_time;
+    MPI_Reduce(&time, &result_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        std::cout << "True: " << TARGET << std::endl;
+        std::cout << "Integral: " << result << std::endl;
+        std::cout << "Eps: " << std::abs(TARGET - result) << std::endl;
+        std::cout << "N points: " << size * block_size * iter << std::endl;
+        std::cout << "Time: " << result_time << std::endl;
     }
-    iter++;
-  }
-  double end_time = MPI_Wtime();
-  double result_time, time = end_time - start_time;
-  MPI_Reduce(&time, &result_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
-    std::cout<<"True: "<<TARGET<<std::endl;
-    std::cout<<"Integral: "<<acc_volume<<std::endl;
-    std::cout<<"Eps: "<<eps<<std::endl;
-    std::cout<<"N points: "<<N * iter<<std::endl;
-    std::cout<<"Time: "<<result_time<<std::endl;
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Finalize();
-  return 0;
+    MPI_Finalize();
+    return 0;
 }
